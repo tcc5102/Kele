5d296f87772435985f73a3337d314f1fc803fb41
Tyler Cooper <tyler.cooper91@gmail.com>
2016-10-27 11:36:56 -0400

    initialized base uri and user api token

---
+++ lib/kele.rb
@@ -0,0 +1,9 @@
+require "HTTParty"
+class Kele
+  include HTTParty
+  def initialize(email, password)
+    @base_uri = "https://www.bloc.io/api/v1"
+    response = self.class.post("#{@base_uri}/sessions", body: { email: email, password: password })
+    @auth_token = response["auth_token"]
+  end
+end
