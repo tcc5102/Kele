aef5615b1f2e509effc7d45a4608918cfe12d939
Tyler Cooper <tyler.cooper91@gmail.com>
2016-10-28 18:17:24 -0400

    retrieve user info

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,15 @@
 require "HTTParty"
+require "json"
 class Kele
   include HTTParty
   def initialize(email, password)
     @base_uri = "https://www.bloc.io/api/v1"
     response = self.class.post("#{@base_uri}/sessions", body: { email: email, password: password })
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get("#{@base_uri}/users/me", headers: { "authorization" => @auth_token })
+    @get_me = JSON.parse(response.body)
   end
 end
